88ba4780de36e7ecf46cb0dfb012e48011673988
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-06-01 10:32:58 +0530

    Create Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="px-6 pt-8 md:px-16 lg:px-36 w-full bg-black text-gray-300">
+            <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-10">
+                <div className="md:max-w-96">
+                    <img
+                        className="w-36 h-auto invert"
+                        src="logo.png"
+                        alt="logo"
+                    />
+                    <p className="mt-6 text-sm">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Voluptate doloremque commodi excepturi nesciunt
+                        eaque ex. Quia qui inventore minima ratione deleniti
+                        aperiam at quas ea sequi, exercitationem sint amet
+                        saepe?
+                    </p>
+                </div>
+                <div className="flex-1 flex items-start md:justify-end gap-20 md:gap-40">
+                    <div>
+                        <h2 className="font-semibold mb-5">Train.ai</h2>
+                        <ul className="text-sm space-y-2">
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">About us</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact us</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy policy</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 className="font-semibold mb-5">Get in touch</h2>
+                        <div className="text-sm space-y-2">
+                            <p>+91 12345 67890</p>
+                            <p>train.support@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p className="pt-4 text-center text-sm pb-5">
+                Copyright {new Date().getFullYear()} © Train.ai. All Right
+                Reserved.
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
