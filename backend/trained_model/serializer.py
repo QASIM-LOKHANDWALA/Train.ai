535b457796c035d536b59548ace003b4b0ac8a85
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-06-09 16:43:10 +0530

    Create Serializer For Graph Ans Stats Model

--- backend/trained_model/serializer.py
+++ backend/trained_model/serializer.py
@@ -1,7 +1,41 @@
 from rest_framework import serializers
-from .models import TrainedModel
+from .models import TrainedModel, ModelStats, ModelGraph
+
+from rest_framework import serializers
+from .models import TrainedModel, ModelStats, ModelGraph
+
+class ModelStatsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ModelStats
+        fields = [
+            'id',
+            'trained_model',
+            'r2_score',
+            'mse',
+            'mae',
+            'accuracy',
+            'precision',
+            'recall',
+            'f1_score'
+        ]
+
+class ModelGraphSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ModelGraph
+        fields = [
+            'id',
+            'trained_model',
+            'title',
+            'description',
+            'graph_image',
+            'graph_json'
+        ]
+        
 
 class TrainedModelSerializer(serializers.ModelSerializer):
+    stats = ModelStatsSerializer(read_only=True)
+    graphs = ModelGraphSerializer(many=True, read_only=True)
+
     class Meta:
         model = TrainedModel
         fields = [
@@ -9,11 +43,14 @@ class TrainedModelSerializer(serializers.ModelSerializer):
             'user_id',
             'model_name',
             'model_type',
+            'polynomial_degree',
             'target_column',
             'features',
             'model_file',
             'created_at',
             'is_public',
-            'likes'
+            'likes',
+            'stats', 
+            'graphs',
         ]
-        read_only_fields = ['id', 'created_at']
+        read_only_fields = ['id', 'created_at', 'stats', 'graphs']
